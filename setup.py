bdb3a9c793718b9c0fc09f9ce6ab1bd64402548e
Manu <bionikspoon@gmail.com>
2015-04-15 16:11:19 -0500

    updated classifiers

--- setup.py
+++ setup.py
@@ -67,16 +67,21 @@ setup(
     cmdclass={'test': PyTest},
     tests_require=['pytest', 'mock'],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Database',
+        'Topic :: Software Development',
+        'Topic :: Utilities'
+
+
     ],
 )
 # @:on
